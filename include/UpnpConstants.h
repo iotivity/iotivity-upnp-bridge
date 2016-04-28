9951d4352d913105a1f293f870cc0957d52180c2
Rick Bell <richard.s.bell@intel.com>
2016-04-28 12:02:53 -0700

    Update the UpnpContasts with AV Devices and Services
    Change-Id: Ie6b539309ee99b6804fee93db27ef70fc13aa67f
    Signed-off-by: Rick Bell <richard.s.bell@intel.com>

--- include/UpnpConstants.h
+++ include/UpnpConstants.h
@@ -27,20 +27,34 @@
 // i.e. vetted by OIC and UPNP. Each section needs TBD and expanded.
 
 // Resource type: devices
-static const std::string UPNP_OIC_TYPE_DEVICE_LIGHT            = "oic.d.light";
-static const std::string UPNP_OIC_TYPE_DEVICE_FAN              = "oic.d.fan";
-static const std::string UPNP_OIC_TYPE_DEVICE_INET_GATEWAY     = "oic.d.inet.gateway";
+static const std::string UPNP_OIC_TYPE_DEVICE_LIGHT             	= "oic.d.light";
+static const std::string UPNP_OIC_TYPE_DEVICE_FAN               	= "oic.d.fan";
+static const std::string UPNP_OIC_TYPE_DEVICE_INET_GATEWAY      	= "oic.d.inet.gateway";
+static const std::string UPNP_OIC_TYPE_DEVICE_MEDIA_RENDERER    	= "oic.d.media.renderer";
+static const std::string UPNP_OIC_TYPE_DEVICE_MEDIA_SERVER      	= "oic.d.media.server";
 
 // Resource type: services
-static const std::string UPNP_OIC_TYPE_BRIGHTNESS    = "oic.r.light.brightness";
-static const std::string UPNP_OIC_TYPE_DIMMING       = "oic.r.light.dimming";
-static const std::string UPNP_OIC_TYPE_POWER_SWITCH  = "oic.r.switch.binary";
+static const std::string UPNP_OIC_TYPE_BRIGHTNESS               	= "oic.r.light.brightness";
+static const std::string UPNP_OIC_TYPE_DIMMING                  	= "oic.r.light.dimming";
+static const std::string UPNP_OIC_TYPE_POWER_SWITCH             	= "oic.r.switch.binary";
+static const std::string UPNP_OIC_TYPE_CONNECTION_MANAGER       	= "oic.r.connection.manager";
+static const std::string UPNP_OIC_TYPE_AV_TRANSPORT             	= "oic.r.av.transport";
+static const std::string UPNP_OIC_TYPE_CONTENT_DIRECTORY        	= "oic.r.content.directory";
+static const std::string UPNP_OIC_TYPE_RENDERING_CONTROL       		= "oic.r.rendering.control";
+static const std::string UPNP_OIC_TYPE_SCHEDULED_RECORDING      	= "oic.r.scheduled.recording";
 
 // URI prefix (URI = URI_PREFIX + UDN)
-static const std::string UPNP_OIC_URI_PREFIX_LIGHT = "/oic/upnp/light/";
-static const std::string UPNP_OIC_URI_PREFIX_DIMMING = "/oic/upnp/dimming/";
-static const std::string UPNP_OIC_IRI_PREFIX_BRIGHTNESS = "/oic/upnp/brightness/";
-static const std::string UPNP_OIC_URI_PREFIX_POWER_SWITCH = "/oic/upnp/switch/";
-static const std::string UPNP_OIC_URI_PREFIX_INET_GATEWAY = "/oic/upnp/igd/";
+static const std::string UPNP_OIC_URI_PREFIX_LIGHT              	= "/oic/upnp/light/";
+static const std::string UPNP_OIC_URI_PREFIX_DIMMING				= "/oic/upnp/dimming/";
+static const std::string UPNP_OIC_IRI_PREFIX_BRIGHTNESS				= "/oic/upnp/brightness/";
+static const std::string UPNP_OIC_URI_PREFIX_POWER_SWITCH 			= "/oic/upnp/switch/";
+static const std::string UPNP_OIC_URI_PREFIX_INET_GATEWAY       	= "/oic/upnp/igd/";
+static const std::string UPNP_OIC_URI_PREFIX_MEDIA_RENDERER     	= "/oic/upnp/media/renderer/";
+static const std::string UPNP_OIC_URI_PREFIX_MEDIA_SERVER       	= "/oic/upnp/media/server/";
+static const std::string UPNP_OIC_URI_PREFIX_CONNECTION_MANAGER		= "/oic/upnp/connection/manager";
+static const std::string UPNP_OIC_URI_PREFIX_AV_TRANSPORT       	= "/oic/upnp/avtransport";
+static const std::string UPNP_OIC_URI_PREFIX_CONTENT_DIRECTORY  	= "/oic/upnp/content/directory";
+static const std::string UPNP_OIC_URI_PREFIX_RENDERING_CONTROL  	= "/oic/upnp/rendering/control";
+static const std::string UPNP_OIC_URI_PREFIX_SCHEDULED_RECORDING	= "/oic/upnp/scheduled/recording";
 
 #endif
