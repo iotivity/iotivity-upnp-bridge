1ea1d4ea6af084383832516f794d72130b0213a9
Rick Bell <richard.s.bell@intel.com>
2016-04-29 14:30:10 -0700

    Add Rendering Control Service Shell
    Change-Id: Ieaf3d2541a1b50bfb56c9ee27d062835f7590b30
    Signed-off-by: Rick Bell <richard.s.bell@intel.com>

---
+++ src/UpnpRenderingControlService.cpp
@@ -0,0 +1,88 @@
+//******************************************************************
+//
+// Copyright 2016 Intel Corporation All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#include "UpnpRenderingControlService.h"
+
+using namespace OIC::Service;
+
+static const string MODULE = "UpnpRenderingControlService";
+
+// Attribute map: "attribute name" -> GET/SET request handlers
+map <const string, pair <UpnpRenderingControl::GetAttributeHandler, UpnpRenderingControl::SetAttributeHandler>> UpnpRenderingControl::AttributeMap =
+{
+//    {"value", {&UpnpRenderingControl::getTarget, &UpnpRenderingControl::setTarget}}
+};
+
+// TODO Implement various OCF attributes/UPnP Actions
+
+bool UpnpRenderingControl::getAttributesRequest(UpnpRequest *request)
+{
+    bool status = false;
+
+    for (auto it = this->AttributeMap.begin(); it != this->AttributeMap.end(); ++it)
+    {
+        DEBUG_PRINT(" \"" << it->first << "\"");
+        // Check the request
+        if (!UpnpAttribute::isValidRequest(&m_attributeMap, it->first, UPNP_ACTION_GET))
+        {
+            request->done++;
+            continue;
+        }
+
+        GetAttributeHandler fp = it->second.first;
+        GUPnPServiceProxyAction *action = (this->*fp)(request);
+        status |= (action != NULL);
+        if (action == NULL)
+        {
+            request->done++;
+        }
+    }
+    return status;
+}
+
+bool UpnpRenderingControl::setAttributesRequest(const RCSResourceAttributes &value, UpnpRequest *request)
+{
+    bool status = false;
+
+    for (auto it = value.begin(); it != value.end(); ++it)
+    {
+        const std::string attrName = it->key();
+
+        DEBUG_PRINT(" \"" << attrName << "\"");
+
+        // Check the request
+        if (!UpnpAttribute::isValidRequest(&m_attributeMap, attrName, UPNP_ACTION_POST))
+        {
+            request->done++;
+            continue;
+        }
+        RCSResourceAttributes::Value attrValue = it->value();
+
+        SetAttributeHandler fp = this->AttributeMap[attrName].second;
+        GUPnPServiceProxyAction *action = (this->*fp)(attrValue, request);
+        status |= (action != NULL);
+        if (action == NULL)
+        {
+            request->done++;
+        }
+    }
+
+    return status;
+}
---
+++ src/UpnpRenderingControlService.h
@@ -0,0 +1,60 @@
+//******************************************************************
+//
+// Copyright 2016 Intel Corporation All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef UPNP_RENDERING_CONTROL_H_
+#define UPNP_RENDERING_CONTROL_H_
+
+#include <string>
+#include <map>
+
+#include <gupnp.h>
+
+#include "UpnpResource.h"
+#include "UpnpInternal.h"
+#include "UpnpService.h"
+
+using namespace std;
+
+class UpnpRenderingControl: public UpnpService
+{
+    friend class UpnpService;
+
+public:
+    typedef GUPnPServiceProxyAction* (UpnpRenderingControl::*GetAttributeHandler)(UpnpRequest*);
+    typedef GUPnPServiceProxyAction* (UpnpRenderingControl::*SetAttributeHandler)(RCSResourceAttributes::Value&, UpnpRequest*);
+
+    UpnpRenderingControl(GUPnPServiceInfo *serviceInfo,
+                    UpnpRequestState *requestState):
+        UpnpService(serviceInfo, UPNP_OIC_TYPE_RENDERING_CONTROL, requestState,
+                    nullptr) // TODO change to Reference to Attribute table)
+    {
+    }
+
+    // TODO Implement various OCF attributes/UPnP Actions
+
+private:
+    static map <const string, pair <GetAttributeHandler, SetAttributeHandler>> AttributeMap;
+
+    bool getAttributesRequest(UpnpRequest *request);
+    bool setAttributesRequest(const RCSResourceAttributes& attrs, UpnpRequest *request);
+
+};
+
+#endif
