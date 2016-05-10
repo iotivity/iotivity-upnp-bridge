7462eb4a0fefd940678b1b44b5bbea984bd73944
George Nash <george.nash@intel.com>
2016-05-10 15:49:17 -0700

    Initial work on commandline application iotApp
    this application is designed to eventually replace
    Client and SimpleClient for testing, verification,
    and discovery.
    
    Change-Id: I5ebb0255c5895e18f4c3fd272aacc514903c2733
    Signed-off-by: George Nash <george.nash@intel.com>

---
+++ examples/iotApp/IotivityUtility.h
@@ -0,0 +1,84 @@
+//******************************************************************
+//
+// Copyright 2016 Intel Corporation All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef IOTIVITYUTILITY_H_
+#define IOTIVITYUTILITY_H_
+
+#include <OCResource.h>
+#include <iostream>
+#include <mutex>
+
+// mutex used in a lock_guard to prevent multiple items printing at the same time making the output
+// unreadable
+std::mutex print_mutex;
+
+void printResourceInformation(std::shared_ptr< OC::OCResource > resource) {
+    std::lock_guard<std::mutex> lock(print_mutex);
+    try {
+            std::cout << "------------------------------------------------------" << std::endl;
+            std::cout << "DISCOVERED Resource:" << std::endl;
+            std::cout << "\tResource address: " << resource->host() << std::endl;
+            std::cout << "\tResource uri: " << resource->uri() << std::endl;
+            std::cout << "\tList of resource interfaces: " << std::endl;
+            for (auto &resourceInterface : resource->getResourceInterfaces())
+            {
+                std::cout << "\t\t" << resourceInterface << std::endl;
+            }
+
+            std::cout << "\tList of resource types: " << std::endl;
+            for (auto &resourceType : resource->getResourceTypes())
+            {
+                std::cout << "\t\t" << resourceType << std::endl;
+            }
+        } catch (std::exception &e) {
+            std::cerr << "Caught exception in printResourceInformation: " << e.what() << std::endl;
+        }
+}
+
+void printResourceCompact(std::shared_ptr< OC::OCResource > resource) {
+    std::lock_guard<std::mutex> lock(print_mutex);
+    try {
+        std::cout << "\t" << resource->host() << resource->uri() << std::endl // uri is links.href
+                << "\tresourceTypes ";
+        for(auto rt : resource->getResourceTypes()) {
+            std::cout << rt << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "\tresourceInterfaces ";
+        for(auto ri : resource->getResourceInterfaces()) {
+            std::cout << ri << " ";
+        }
+        std::cout << std::endl;
+        std:: cout << "\tsid " << resource->sid() << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << "Caught exception in printResourceInformation: " << e.what() << std::endl;
+    }
+}
+
+// Comparison operator used to insert the OCResources into the sets.
+struct OCResourceComp
+{
+    bool operator()(std::shared_ptr<OC::OCResource> lhs, std::shared_ptr<OC::OCResource> rhs) const
+    {
+        return (*lhs) < (*rhs);
+    }
+};
+
+#endif /* EXAMPLES_IOTAPP_IOTIVITYUTILITY_H_ */
---
+++ examples/iotApp/MenuServices.h
@@ -0,0 +1,328 @@
+//******************************************************************
+//
+// Copyright 2016 Intel Corporation All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef MENUSERVICES_H_
+#define MENUSERVICES_H_
+
+#include <OCApi.h>
+#include <OCPlatform.h>
+
+#include <functional>
+#include <mutex>
+
+#include "IotivityUtility.h"
+#include "MenuBase.h"
+#include "MenuBinarySwitch.h"
+#include "MenuBrightness.h"
+
+class MenuServices: public MenuBase
+{
+public:
+    MenuServices();
+    virtual ~MenuServices();
+    virtual std::string getName();
+    virtual void help();
+    virtual void print();
+    virtual void run(const std::vector<std::string>& cmd, std::stack<std::unique_ptr<MenuBase>>& menuStack);
+    virtual bool quit();
+private:
+    void onFindResource(std::shared_ptr< OC::OCResource > resource);
+
+    std::set<std::shared_ptr<OC::OCResource>, OCResourceComp> m_avTransport;
+    std::set<std::shared_ptr<OC::OCResource>, OCResourceComp> m_binarySwitch;
+    std::set<std::shared_ptr<OC::OCResource>, OCResourceComp> m_brightness;
+    std::set<std::shared_ptr<OC::OCResource>, OCResourceComp> m_connectionManager;
+    std::set<std::shared_ptr<OC::OCResource>, OCResourceComp> m_contentDirectory;
+    std::set<std::shared_ptr<OC::OCResource>, OCResourceComp> m_layer3Forwarding;
+    std::set<std::shared_ptr<OC::OCResource>, OCResourceComp> m_renderingControl;
+    std::set<std::shared_ptr<OC::OCResource>, OCResourceComp> m_schedualedRecording;
+    std::set<std::shared_ptr<OC::OCResource>, OCResourceComp> m_wanIfConfig;
+
+
+    bool m_quit;
+    std::mutex m_mutex;
+
+    OC::FindCallback onFindResourceCb;
+};
+
+MenuServices::MenuServices():
+        m_quit(false)
+{
+    onFindResourceCb = std::bind(&MenuServices::onFindResource, this, std::placeholders::_1);
+    print();
+}
+
+MenuServices::~MenuServices() {}
+
+std::string MenuServices::getName()
+{
+    return "Services";
+}
+
+void MenuServices::print()
+{
+    // Services that are unimplemented are currently commented out these can quickly be added as
+    // the menu is created.
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << getName() << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "find) find all known services" << std::endl;
+    std::cout << "list) list all found services" << std::endl;
+    //std::cout << "av) " << UPNP_OIC_TYPE_AV_TRANSPORT << " (" << m_avTransport.size() << " found)" << std::endl;
+    std::cout << "brightness) " << UPNP_OIC_TYPE_BRIGHTNESS << " (" << m_brightness.size() << " found)" << std::endl;
+    std::cout << "switch) " << UPNP_OIC_TYPE_POWER_SWITCH << " (" << m_binarySwitch.size() << " found)" << std::endl;
+    //std::cout << "connection) " << UPNP_OIC_TYPE_CONNECTION_MANAGER << " (" << m_connectionManager.size() << " found)" << std::endl;
+    //std::cout << "content) " << UPNP_OIC_TYPE_CONTENT_DIRECTORY << " (" << m_contentDirectory.size() << " found)" << std::endl;
+    //std::cout << "layer3) " << UPNP_OIC_TYPE_LAYER3_FORWARDING << " (" << m_layer3Forwarding.size() << " found)" << std::endl;
+    //std::cout << "rendering) " << UPNP_OIC_TYPE_RENDERING_CONTROL << " (" << m_renderingControl.size() << " found)" << std::endl;
+    //std::cout << "recording) " << UPNP_OIC_TYPE_SCHEDULED_RECORDING << " (" << m_renderingControl.size() << " found)" << std::endl;
+    //std::cout << "ifconfig) " << UPNP_OIC_TYPE_WAN_IF_CONFIG << " (" << m_wanIfConfig.size() << " found)" << std::endl;
+    std::cout << "clear) clear all found services" << std::endl;
+    std::cout << "b) Back" << std::endl;
+    std::cout << "h) Help" << std::endl;
+    std::cout << "q) Exit" << std::endl;
+}
+
+void MenuServices::help() {
+    print();
+}
+void MenuServices::run(const std::vector<std::string>& cmd, std::stack<std::unique_ptr<MenuBase>>& menuStack)
+{
+    if(cmd.size() > 0) {
+        if (cmd[0] == "find") {
+            std::vector<std::string> knownServices = {
+                    // services
+                    std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_BRIGHTNESS,
+                    std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_POWER_SWITCH,
+                    std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_CONNECTION_MANAGER,
+                    std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_AV_TRANSPORT,
+                    std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_CONTENT_DIRECTORY,
+                    std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_RENDERING_CONTROL,
+                    std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_SCHEDULED_RECORDING,
+                    std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_WAN_IF_CONFIG,
+                    std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_LAYER3_FORWARDING,
+            };
+            for (auto service : knownServices) {
+                OCStackResult result = OC::OCPlatform::findResource("", service, CT_DEFAULT, onFindResourceCb);
+                std::cout << "findResource(" + service + ") - " << result << std::endl;
+            }
+
+        } else if (cmd[0] == "list") {
+            if(!m_avTransport.empty()) {
+                std::cout << "AV Transport Service(s) found:" << std::endl;
+                std::cout << "---------------------------------------------" << std::endl;
+                for( auto r : m_avTransport) {
+                    printResourceCompact(r);
+                    std::cout << std::endl;
+                }
+            }
+            if(!m_brightness.empty()) {
+                std::cout << "Brightness Service(s) found:" << std::endl;
+                std::cout << "---------------------------------------------" << std::endl;
+                for( auto r : m_brightness) {
+                    printResourceCompact(r);
+                    std::cout << std::endl;
+                }
+            }
+            if(!m_binarySwitch.empty()) {
+                std::cout << "Binary Switch Service(s) found:" << std::endl;
+                std::cout << "---------------------------------------------" << std::endl;
+                for( auto r : m_binarySwitch) {
+                    printResourceCompact(r);
+                    std::cout << std::endl;
+                }
+            }
+            if(!m_connectionManager.empty()) {
+                std::cout << "Connection Manager Service(s) found:" << std::endl;
+                std::cout << "---------------------------------------------" << std::endl;
+                for( auto r : m_connectionManager) {
+                    printResourceCompact(r);
+                    std::cout << std::endl;
+                }
+            }
+            if(!m_contentDirectory.empty()) {
+                std::cout << "Content Dirctory Service(s) found:" << std::endl;
+                std::cout << "---------------------------------------------" << std::endl;
+                for( auto r : m_contentDirectory) {
+                    printResourceCompact(r);
+                    std::cout << std::endl;
+                }
+            }
+            if(!m_layer3Forwarding.empty()) {
+                std::cout << "Layer 3 Forwarding Service(s) found:" << std::endl;
+                std::cout << "---------------------------------------------" << std::endl;
+                for( auto r : m_layer3Forwarding) {
+                    printResourceCompact(r);
+                    std::cout << std::endl;
+                }
+            }
+            if(!m_renderingControl.empty()) {
+                std::cout << "Rendering Control Service(s) found:" << std::endl;
+                std::cout << "---------------------------------------------" << std::endl;
+                for( auto r : m_renderingControl) {
+                    printResourceCompact(r);
+                    std::cout << std::endl;
+                }
+            }
+            if(!m_schedualedRecording.empty()) {
+                std::cout << "Scheduled Recording Service(s) found:" << std::endl;
+                std::cout << "---------------------------------------------" << std::endl;
+                for( auto r : m_schedualedRecording) {
+                    printResourceCompact(r);
+                    std::cout << std::endl;
+                }
+            }
+            if(!m_wanIfConfig.empty()) {
+                std::cout << "WAN IF Config Service(s) found:" << std::endl;
+                std::cout << "---------------------------------------------" << std::endl;
+                for( auto r : m_wanIfConfig) {
+                    printResourceCompact(r);
+                    std::cout << std::endl;
+                }
+            }
+        } else if("av" == cmd[0]) {
+            std::cout << "AV Transport Menu not yet Implemented" << std::endl;
+        } else if("brightness" == cmd[0]) {
+            std::cout << "Brightness Menu not yet Implemented" << std::endl;
+            std::cout << "Binary Switch Menu not yet Implemented" << std::endl;
+            std::unique_ptr<MenuBrightness> menuItem(new MenuBrightness);
+            menuItem->init(m_brightness);
+            menuStack.push(std::move(menuItem));
+        } else if("switch" == cmd[0]) {
+            std::cout << "Binary Switch Menu not yet Implemented" << std::endl;
+            std::unique_ptr<MenuBinarySwitch> menuItem(new MenuBinarySwitch);
+            menuItem->init(m_binarySwitch);
+            menuStack.push(std::move(menuItem));
+        } else if("connection" == cmd[0]) {
+            std::cout << "Connection Manager Menu not yet Implemented" << std::endl;
+        } else if("content" == cmd[0]) {
+            std::cout << "Content directory Menu not yet Implemented" << std::endl;
+        } else if("layer3" == cmd[0]) {
+            std::cout << "Layer 3 Forwarding Menu not yet Implemented" << std::endl;
+        } else if("rendering" == cmd[0]) {
+            std::cout << "Rendering Control Menu not yet Implemented" << std::endl;
+        } else if("recording" == cmd[0]) {
+            std::cout << "Scheduled Recording Menu not yet Implemented" << std::endl;
+        } else if("ifconfig" == cmd[0]) {
+            std::cout << "WAN IF Config Menu not yet Implemented" << std::endl;
+        } else if ("clear" == cmd[0]) {
+            m_avTransport.clear();
+            m_binarySwitch.clear();
+            m_brightness.clear();
+            m_connectionManager.clear();
+            m_contentDirectory.clear();
+            m_layer3Forwarding.clear();
+            m_renderingControl.clear();
+            m_schedualedRecording.clear();
+            m_wanIfConfig.clear();
+            print();
+        } else if("q" == cmd[0]) {
+            m_quit = true;
+        } else if("b" == cmd[0]) {
+            if(menuStack.size() <= 1)
+            {
+                print();
+                return; //do nothing
+            } else {
+                menuStack.pop();
+                menuStack.top()->print();
+            }
+        } else if("h" == cmd[0]) {
+            help();
+        }
+    }
+}
+
+bool MenuServices::quit() {
+    return m_quit;
+}
+
+void MenuServices::onFindResource(std::shared_ptr< OC::OCResource > resource)
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+    std::cout << "Found resource" << std::endl;
+    printResourceCompact(resource);
+
+    try
+        {
+            if (resource)
+            {
+                for (auto &resourceType : resource->getResourceTypes())
+                {
+                    bool isNewServiceFound = false;
+                    if(resourceType == UPNP_OIC_TYPE_AV_TRANSPORT) {
+                         if(m_avTransport.find(resource) == m_avTransport.end()) {
+                             m_avTransport.insert(resource);
+                             isNewServiceFound = true;
+                         }
+                     } else if(resourceType == UPNP_OIC_TYPE_POWER_SWITCH) {
+                        if(m_binarySwitch.find(resource) == m_binarySwitch.end()) {
+                            m_binarySwitch.insert(resource);
+                            isNewServiceFound = true;
+                        }
+                    } else if(resourceType == UPNP_OIC_TYPE_BRIGHTNESS) {
+                        if(m_brightness.find(resource) == m_brightness.end()) {
+                            m_brightness.insert(resource);
+                            isNewServiceFound = true;
+                        }
+                    } else if(resourceType == UPNP_OIC_TYPE_CONNECTION_MANAGER) {
+                        if(m_connectionManager.find(resource) == m_connectionManager.end()) {
+                            m_connectionManager.insert(resource);
+                            isNewServiceFound = true;
+                        }
+                    } else if(resourceType == UPNP_OIC_TYPE_CONTENT_DIRECTORY) {
+                        if(m_contentDirectory.find(resource) == m_contentDirectory.end()) {
+                            m_contentDirectory.insert(resource);
+                            isNewServiceFound = true;
+                        }
+                    } else if(resourceType == UPNP_OIC_TYPE_LAYER3_FORWARDING) {
+                        if(m_layer3Forwarding.find(resource) == m_layer3Forwarding.end()) {
+                            m_layer3Forwarding.insert(resource);
+                            isNewServiceFound = true;
+                        }
+                    } else if(resourceType == UPNP_OIC_TYPE_RENDERING_CONTROL) {
+                        if(m_renderingControl.find(resource) == m_renderingControl.end()) {
+                            m_renderingControl.insert(resource);
+                            isNewServiceFound = true;
+                        }
+                    } else if(resourceType == UPNP_OIC_TYPE_SCHEDULED_RECORDING) {
+                        if(m_schedualedRecording.find(resource) == m_schedualedRecording.end()) {
+                            m_schedualedRecording.insert(resource);
+                            isNewServiceFound = true;
+                        }
+                    } else if(resourceType == UPNP_OIC_TYPE_WAN_IF_CONFIG) {
+                        if(m_wanIfConfig.find(resource) == m_wanIfConfig.end()) {
+                            m_wanIfConfig.insert(resource);
+                            isNewServiceFound = true;
+                        }
+                    }
+                    if(isNewServiceFound) {
+                        print();
+                    }
+                }
+            }
+        }
+        catch (std::exception &e)
+        {
+            std::cerr << "Caught exception in foundResource: " << e.what() << std::endl;
+        }
+}
+
+#endif /* MENUSERVICES_H_ */
