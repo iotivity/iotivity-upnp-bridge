af00b3b286e1199d5c78a37ca7d8d551ed322b56
George Nash <george.nash@intel.com>
2016-05-19 13:21:51 -0700

    Add option to run iotApp in loop for testing
    Change-Id: I515dc00c6969877961d17789dc7060a0c3763da1
    Signed-off-by: George Nash <george.nash@intel.com>

--- examples/iotApp/CommandlineMode.h
+++ examples/iotApp/CommandlineMode.h
@@ -36,12 +36,16 @@ public:
     CommandlineMode();
     void run(int argc, char *argv[]);
 private:
+    void printHelp();
     void foundResource(std::shared_ptr< OC::OCResource > resource);
     void testBrightness(std::shared_ptr< OC::OCResource > resource);
     void testSwitch(std::shared_ptr< OC::OCResource > resource);
     bool listAllDevices;
     bool listAllServices;
     bool runTest;
+    bool runInLoop;
+    int  loopCount;
+    int  loopDelayTime;
     int waitTime;
 
     std::shared_ptr<OC::OCResource> m_devices;
@@ -57,6 +61,9 @@ CommandlineMode::CommandlineMode() :
     listAllDevices(false),
     listAllServices(false),
     runTest(false),
+    runInLoop(false),
+    loopCount(5),
+    loopDelayTime(10),
     waitTime(-1){
 
     onFoundCB        = std::bind(&CommandlineMode::foundResource, this, std::placeholders::_1);
@@ -71,31 +78,110 @@ void CommandlineMode::run(int argc, char *argv[])
     bool listAllServices = false;
     /* Parse command line args */
     for (int i = 1; i < argc; ++i) {
-        if(0 == strcmp("--list", argv[i])) {
+        // currently all commands should be at least 2 characters
+        if(strlen(argv[i]) < 2) {
+            std::cerr << "Invalid input option" << std::endl;
+            printHelp();
+            exit(0);
+        }
+        // split the input so we can check for the "=" character for options
+        // that take the "=" character.
+        char* token = strtok(argv[i], "= ");
+        std::cout << "token>> " << token << std::endl;
+        if (0 == strcmp("--help", token) || 0 == strcmp("-h", token)) {
+            printHelp();
+            exit(0);
+        } else if (0 == strcmp("--list", token)) {
             listAllDevices = true;
             listAllServices = true;
-        }
-        if(0 == strcmp("--time", argv[i])) {
-            if(i < argc) {
-                ++i;
-                waitTime = std::stoi(argv[i]);
-                //std::cout << "Waiting " << waitTime << " seconds before exiting." << std::endl;
+        } else if (0 == strncmp("--time", token, strlen("--time"))) {
+            token = strtok(nullptr, "=");
+            try {
+                waitTime = std::stoi(token);
+                std::cout << "Waiting " << waitTime << " seconds before exiting." << std::endl;
+            } catch (const std::invalid_argument& ia) {
+                std::cerr << "Invalid input option for --loop option" << std::endl;
+                printHelp();
+                exit(0);
             }
-        }
-        if(0 == strcmp("--test", argv[i])) {
+        } else if (0 == strcmp("--test", token)) {
             runTest = true;
+        } else if (0 == strcmp("--loop", token)) {
+            runInLoop = true;
+            token = strtok(nullptr, "=");
+            if (token != nullptr) {
+                if (token[0] != '-') {
+                    try {
+                        loopCount = std::stoi(token);
+                    } catch (const std::invalid_argument& ia) {
+                        std::cerr << "Invalid input option for --loop option" << std::endl;
+                        printHelp();
+                        exit(0);
+                    }
+                } else {
+                    loopCount = -1; //use -1 to indicate infinite loops
+                }
+            } else {
+                loopCount = -1; //use -1 to indicate infinite loops
+            }
+        } else if (0 == strcmp("--delay", token)) {
+            token = strtok(nullptr, "=");
+            if (nullptr != token) {
+                if (token[0] != '-') {
+                    try {
+                        loopDelayTime = std::stoi(token);
+                    } catch (const std::invalid_argument& ia) {
+                        std::cerr << "Invalid input option for --delay option" << std::endl;
+                        printHelp();
+                        exit(0);
+                    }
+                } else {
+                    std::cerr << "--delay option can not have negative time" << std::endl;
+                    printHelp();
+                    exit(0);
+                }
+            } else {
+                std::cerr << "Missing time for --delay option" << std::endl;
+                printHelp();
+                exit(0);
+            }
         }
 
     }
-    if(listAllDevices || listAllServices) {
-        std::cout << "Finding all Resources."  << std::endl;
-        OC::OCPlatform::findResource("", OC_RSRVD_WELL_KNOWN_URI, CT_DEFAULT, onFoundCB);
+
+    std::cout << "--------------------------------------------------" << std::endl;
+    std::cout << " Run test : " << ((runTest) ? "TRUE" : "FALSE") << std::endl;
+    std::cout << " List Discovered resources " << ((listAllDevices || listAllServices) ? "TRUE" : "FALSE") << std::endl;
+    std::cout << " Loop Mode: " << ((runInLoop) ? "TRUE" : "FALSE") <<
+                 " LoopCount: ";
+    ((loopCount == -1) ? std::cout << "ENDLESS" : std::cout << loopCount);
+    std::cout << " LoopDelay: " << loopDelayTime << std::endl;
+    if(loopCount != -1) { // The program is in endless mode SIGINT required to exit.
+        if (waitTime == -1) {
+            std::cout <<" Program will wait for 'q' to exit" << std::endl;
+        } else {
+            std::cout << " Wait " << waitTime << " seconds then exit" << std::endl;
+        }
     }
-    // Run pre-programmed test code that is used to verify functionality of found resource
-    if(runTest) {
-        OC::OCPlatform::findResource("", std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_BRIGHTNESS, CT_DEFAULT, testBrightnessCB);
-        OC::OCPlatform::findResource("", std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_POWER_SWITCH, CT_DEFAULT, testSwitchCB);
-    }
+    std::cout << "--------------------------------------------------" << std::endl;
+    size_t loopIndicator = 1;
+    do {
+        if(runInLoop) {
+            std::cout << "[LOOP " << loopIndicator++ << "]" << std::endl;
+        }
+        if(listAllDevices || listAllServices) {
+            std::cout << "Finding all Resources."  << std::endl;
+            OC::OCPlatform::findResource("", OC_RSRVD_WELL_KNOWN_URI, CT_DEFAULT, onFoundCB);
+        }
+        // Run pre-programmed test code that is used to verify functionality of found resource
+        if(runTest) {
+            OC::OCPlatform::findResource("", std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_BRIGHTNESS, CT_DEFAULT, testBrightnessCB);
+            OC::OCPlatform::findResource("", std::string(OC_RSRVD_WELL_KNOWN_URI) +  "?rt=" + UPNP_OIC_TYPE_POWER_SWITCH, CT_DEFAULT, testSwitchCB);
+        }
+        if(runInLoop) {
+            sleep(loopDelayTime);
+        }
+    } while (runInLoop && (loopCount == -1 || --loopCount > 0));
     if (-1 == waitTime) {
         std::cout << "Enter 'q' to exit program." << std::endl;
         while (getchar() != 'q');
@@ -104,6 +190,32 @@ void CommandlineMode::run(int argc, char *argv[])
         sleep(waitTime); //wait x seconds then exit
         std::cout << "EXITING" << std::endl;
     }
+}
+
+void CommandlineMode::printHelp() {
+    std::cout <<
+            "Usage: iotApp [OPTION]...\n"
+            "\n"
+            "The iotApp runs in two modes: interactive mode and commandline mode. To enter\n"
+            "interactive mode run iotApp with no additional arguments. This will print text driven\n"
+            "menus that can be used to find and interact with IoTivity devices and services.\n"
+            "Commandline can run pre-programmed actions for testing and verification of IoTivity\n"
+            "Services.\n"
+            "\n"
+            "Optional arguments for commandline mode are:\n"
+            "---------------------------------------------------------------------------------------\n"
+            "    --list          Discover and print all found Iotivity resources\n"
+            "    --test          Run pre-programmed test code\n"
+            "    --loop[=count]  Run the program in a loop if count is not given it will run in an\n"
+            "                    endless loop. The program will not exit till an interupt signal is\n"
+            "                    recieved.\n"
+            "    --delay=time    The delay option is used to specify how long the program will wait\n"
+            "                    in seconds between each loop when using the --loop option. If no\n"
+            "                    delay option is specified the delay time will default to 10 seconds\n"
+            "    --time=time     The number of seconds the program will wait for async actions to\n"
+            "                    complete before exiting the program. If no time is given 'q' must\n"
+            "                    be typed to quit the program upon completion.\n"
+            "-h, --help          print this help text." << std::endl;
 }
 
 void CommandlineMode::foundResource(std::shared_ptr< OC::OCResource > resource) {
