b17c87dfb2a1ad037641528ca3f362d5aea86349
George Nash <george.nash@intel.com>
2016-06-15 09:09:26 -0700

    Add Connection Manager code to the iotApp client
    the two menu options for search and browse are currently
    dissabled till more testing is done.
    
    More work may need to be done on the input parser before
    the menu options can be re-enabled.  Currently there is
    no way to input an empty string using the current input
    parser.  There may be instances that the browse and
    search options would take empty strings.
    
    Change-Id: Ie3f4e66dcdac8b17af9539377cfd234049a523a9
    Signed-off-by: George Nash <george.nash@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/8681
    Reviewed-by: Rick Bell <richard.s.bell@intel.com>
    Tested-by: Rick Bell <richard.s.bell@intel.com>

---
+++ examples/iotApp/ContentDirectory.h
@@ -0,0 +1,96 @@
+//******************************************************************
+//
+// Copyright 2016 Intel Corporation All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef CONTENTDIRECTORY_H_
+#define CONTENTDIRECTORY_H_
+
+#include <vector>
+#include <string>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+
+#include <OCResource.h>
+#include <OCRepresentation.h>
+
+class ContentDirectory
+{
+    public:
+        ContentDirectory();
+        ContentDirectory(OC::OCResource::Ptr resource);
+        virtual ~ContentDirectory();
+        ContentDirectory(const ContentDirectory &other);
+        ContentDirectory &operator=(const ContentDirectory &other);
+
+        // BrowseResult and SearchResult return the same struct
+        struct SearchResult
+        {
+            std::string result;
+            int numberReturned;
+            int totalMatches;
+            int updateId;
+        };
+
+        std::string getSearchCapabilities();
+        std::string getSortCapabilites();
+        int getSystemUpdateId();
+        std::string getServiceResetToken();
+        std::string getFeatureList();
+        SearchResult browse(std::string objectId, std::string browseFlag, std::string filter,
+                int startingIndex, int requestedCount, std::string sortCriteria);
+        SearchResult search(std::string containerId, std::string searchCriteria, std::string filter,
+                int startingIndex, int requestedCount, std::string sortCriteria);
+        //Overloaded operator used for putting into a 'set'
+        bool operator<(const ContentDirectory &other) const;
+        const std::shared_ptr<OC::OCResource> getResource() const {return m_resource;}
+    private:
+        void onGetSearchCapabilites(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep, const int eCode);
+        void onGetSortCapabilites(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep, const int eCode);
+        void onGetSystemUpdateId(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep, const int eCode);
+        void onGetServiceResetToken(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep, const int eCode);
+        void onGetFeatureList(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep, const int eCode);
+        void onBrowse(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep, const int eCode);
+        void onSearch(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep, const int eCode);
+
+        OC::GetCallback m_getSearchCapabilitiesCB;
+        OC::GetCallback m_getSortCapabilitesCB;
+        OC::GetCallback m_getSystemUpdateIdCB;
+        OC::GetCallback m_getServiceResetTokenCB;
+        OC::GetCallback m_getFeatureListCB;
+        OC::GetCallback m_browseCB;
+        OC::GetCallback m_searchCB;
+
+        // These are only used to hold return values for async calls
+        std::string m_searchCaps;
+        std::string m_sortCaps;
+        int m_id;
+        std::string m_resetToken;
+        std::string m_featureList;
+        SearchResult m_browseResult;
+        SearchResult m_searchResult;
+
+        OC::OCResource::Ptr m_resource;
+
+        //used to turn the async get call to a sync method call
+        std::mutex m_mutex;
+        std::condition_variable m_cv;
+};
+
+#endif /* CONTENTDIRECTORY_H_ */
