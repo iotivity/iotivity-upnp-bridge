f055cd28481715fb5dcee9e3843102eb3c494b36
George Nash <george.nash@intel.com>
2016-07-08 11:30:31 -0700

    Add WANCommonInterfaceService to iotApp
    Change-Id: I9581b4db5a4d4af842519c4395269455faf9f739
    Signed-off-by: George Nash <george.nash@intel.com>

---
+++ examples/iotApp/MenuWANCommonInterface.h
@@ -0,0 +1,60 @@
+//******************************************************************
+//
+// Copyright 2016 Intel Corporation All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef MENUWANCOMMONINTERFACE_H_
+#define MENUWANCOMMONINTERFACE_H_
+
+#include <OCApi.h>
+#include <OCPlatform.h>
+
+#include <set>
+#include <vector>
+
+#include "IotivityUtility.h"
+#include "MenuBase.h"
+
+class MenuWANCommonInterface: public MenuBase
+{
+    public:
+        MenuWANCommonInterface();
+        virtual ~MenuWANCommonInterface();
+        /*
+         * The init member function is optional.
+         * Used to pass in an already discovered AV transport resources before calling run.
+         */
+        void init(std::set<OC::OCResource::Ptr, OCResourceComp> avTransportSet);
+        virtual std::string getName();
+        virtual void help();
+        virtual void print();
+        virtual void run(const std::vector<std::string> &cmd,
+                         std::stack<std::unique_ptr<MenuBase> > &menuStack);
+        virtual bool quit();
+    private:
+        void onFindResource(OC::OCResource::Ptr);
+
+        std::set<OC::OCResource::Ptr, OCResourceComp> m_wanCommonInterfaceSet;
+        std::vector<OC::OCResource::Ptr> m_wanCommonInterfaceVector;
+        bool m_quit;
+        std::mutex m_mutex;
+
+        OC::FindCallback onFindResourceCb;
+};
+
+#endif /* MENUWANCOMMONINTERFACE_H_ */
---
+++ examples/iotApp/WANCommonInterfaceConfig.h
@@ -0,0 +1,127 @@
+//******************************************************************
+//
+// Copyright 2016 Intel Corporation All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef WANCOMMONINTERFACECONFIG_H_
+#define WANCOMMONINTERFACECONFIG_H_
+
+#include <vector>
+#include <string>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+
+#include <OCResource.h>
+#include <OCRepresentation.h>
+
+class WANCommonInterfaceConfig
+{
+    public:
+        WANCommonInterfaceConfig();
+        WANCommonInterfaceConfig(OC::OCResource::Ptr resource);
+        virtual ~WANCommonInterfaceConfig();
+        WANCommonInterfaceConfig(const WANCommonInterfaceConfig &other);
+        WANCommonInterfaceConfig &operator=(const WANCommonInterfaceConfig &other);
+
+        struct LinkProperties
+        {
+            std::string accessType;
+            int upMaxBitrate;
+            int downMaxBitrate;
+            std::string linkStatus;
+        };
+
+        struct ConnectionInfo
+        {
+            std::string deviceContainer;
+            std::string serviceId;
+        };
+
+        void setEnabledForInternet(bool inetEnabled);
+        bool getEnabledForInternet();
+        LinkProperties getCommonLinkProperties();
+        std::string getWANAccessProvider();
+        int getMaximumActiveConnections();
+        int getTotalBytesSent();
+        int getTotalBytesReceived();
+        int getTotalPacketsSent();
+        int getTotalPacketsReceived();
+        //special case: no matching UPNP action, but can be set based on observation
+        int getNumberOfActiveConnections();
+        ConnectionInfo getActiveConnection();
+
+        //Overloaded operator used for putting into a 'set'
+        bool operator<(const WANCommonInterfaceConfig &other) const;
+        const std::shared_ptr<OC::OCResource> getResource() const {return m_resource;}
+    private:
+        void onSetEnabledForInternet(const OC::HeaderOptions &headerOptions,
+                                     const OC::OCRepresentation &rep, const int eCode);
+        void onGetEnabledForInternet(const OC::HeaderOptions &headerOptions,
+                                     const OC::OCRepresentation &rep, const int eCode);
+        void onGetCommonLinkProperties(const OC::HeaderOptions &headerOptions,
+                                       const OC::OCRepresentation &rep, const int eCode);
+        void onGetWANAccessProvider(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep,
+                                    const int eCode);
+        void onGetMaximumActiveConnections(const OC::HeaderOptions &headerOptions,
+                                           const OC::OCRepresentation &rep, const int eCode);
+        void onGetTotalBytesSent(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep,
+                                 const int eCode);
+        void onGetTotalBytesReceived(const OC::HeaderOptions &headerOptions,
+                                     const OC::OCRepresentation &rep, const int eCode);
+        void onGetTotalPacketsSent(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep,
+                                   const int eCode);
+        void onGetTotalPacketsReceived(const OC::HeaderOptions &headerOptions,
+                                       const OC::OCRepresentation &rep, const int eCode);
+        void onGetNumberOfActiveConnections(const OC::HeaderOptions &headerOptions,
+                                            const OC::OCRepresentation &rep, const int eCode);
+        void onGetActiveConnection(const OC::HeaderOptions &headerOptions, const OC::OCRepresentation &rep,
+                                   const int eCode);
+
+        OC::PostCallback m_setEnabledForInternetCB;
+        OC::GetCallback m_getEnabledForInternetCB;
+        OC::GetCallback m_getCommonLinkPropertiesCB;
+        OC::GetCallback m_getWANAccessProviderCB;
+        OC::GetCallback m_getMaximumActiveConnectionsCB;
+        OC::GetCallback m_getTotalBytesSentCB;
+        OC::GetCallback m_getTotalBytesReceivedCB;
+        OC::GetCallback m_getTotalPacketsSentCB;
+        OC::GetCallback m_getTotalPacketsReceivedCB;
+        OC::GetCallback m_getNumberOfActiveConnectionsCB;
+        OC::GetCallback m_getActiveConnectionCB;
+
+        // Used to hold return values for async calls
+        bool m_inetEnabled;
+        LinkProperties m_linkProperties;
+        std::string m_accessProvider;
+        int m_maxActiveConnections;
+        int m_totalBytesSent;
+        int m_totalBytesReceived;
+        int m_totalPacketsSent;
+        int m_totalPacketsReceived;
+        int m_numberOfConnections;
+        ConnectionInfo m_activeConnection;
+
+        OC::OCResource::Ptr m_resource;
+
+        //used to turn the async get call to a sync method call
+        std::mutex m_mutex;
+        std::condition_variable m_cv;
+};
+
+#endif /* WANCOMMONINTERFACECONFIG_H_ */
