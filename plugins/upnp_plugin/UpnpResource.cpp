7a63dde0ecdb7f315d54bb3de656ac4c17e25910
George Nash <george.nash@intel.com>
2017-02-15 13:08:04 -0800

    Initial work to start moving to the MPM
    This setup a fold called plugins that contains
    a single file that is a copy of the stub_plugin
    from the iotivity bridgin folder. With a few minor
    changes.
    
    This commit also contains the build script additions
    to build the plugin.
    
    This does not contain a client to launch the plugin
    the mpm_sample_client from iotivity will be needed
    to start the plugin for the time being.
    
    Change-Id: Ie362541af8f5fcd3983cc8a1514556a2c0c42827
    Signed-off-by: George Nash <george.nash@intel.com>

---
+++ plugins/upnp_plugin/UpnpResource.cpp
@@ -0,0 +1,94 @@
+//******************************************************************
+//
+// Copyright 2016 Intel Corporation All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#include <boost/regex.hpp>
+
+#include "UpnpResource.h"
+#include "UpnpConstants.h"
+#include "UpnpInternal.h"
+
+using namespace std;
+using namespace boost;
+
+static const string MODULE = "UpnpResource";
+
+UpnpResource::UpnpResource()
+{
+    m_ready = false;
+    m_registered = false;
+    m_udn = "";
+    m_links.clear();
+}
+
+UpnpResource::~UpnpResource()
+{
+    m_registered = false;
+    m_ready = false;
+    m_links.clear();
+}
+
+void UpnpResource::setRegistered(bool registered)
+{
+    m_registered = registered;
+}
+
+bool UpnpResource::isRegistered()
+{
+    return m_registered;
+}
+
+void UpnpResource::addLink(UpnpResource::Ptr pResource)
+{
+    RCSResourceAttributes singleLink ;
+
+    singleLink["href"] = pResource->m_uri;
+    singleLink["rel"] = "contains";
+    singleLink["rt"] = pResource->getResourceType();
+
+    m_links.push_back(singleLink);
+}
+
+void UpnpResource::setLinkAttribute()
+{
+    if (!m_links.empty())
+    {
+        DEBUG_PRINT("Setting links");
+        //BundleResource::setAttribute("links", m_links);
+    }
+}
+
+std::string UpnpResource::getResourceType()
+{
+    return m_resourceType;
+}
+
+std::string UpnpResource::getUdn()
+{
+    return m_udn;
+}
+
+void UpnpResource::setReady(bool isReady)
+{
+    m_ready = isReady;
+}
+
+bool UpnpResource::isReady()
+{
+    return m_ready;
+}
