d65dee2dbf150ab2a2492a96ef5ae087e7bade22
George Nash <george.nash@intel.com>
2017-03-23 10:23:33 -0700

    Handle the pluginAdd request
    Change-Id: I0381b03063fb79c823888fd7a257c5684dbf281d
    Signed-off-by: George Nash <george.nash@intel.com>
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

--- plugins/upnp_plugin/UpnpPowerSwitchService.cpp
+++ plugins/upnp_plugin/UpnpPowerSwitchService.cpp
@@ -42,3 +42,62 @@ vector <UpnpAttributeInfo> UpnpPowerSwitch::Attributes =
         {}
     }
 };
+
+static const char* powerSwitchStateName = "value";
+
+OCEntityHandlerResult UpnpPowerSwitch::processGetRequest(OCRepPayload *payload)
+{
+    if (payload == NULL)
+    {
+        throw "payload is null";
+    }
+
+    //TODO read actual value from UPnP light
+
+    if (!OCRepPayloadSetPropBool(payload, powerSwitchStateName, powerSwitchStateValue))
+    {
+        throw "Failed to set power switch value in payload";
+    }
+    DEBUG_PRINT(powerSwitchStateName << ": " << (powerSwitchStateValue ? "true" : "false"));
+
+    return OC_EH_OK;
+}
+
+OCEntityHandlerResult UpnpPowerSwitch::processPutRequest(OCEntityHandlerRequest *ehRequest,
+        string uri, string resourceType, OCRepPayload *payload)
+{
+    if (!ehRequest || !ehRequest->payload ||
+            ehRequest->payload->type != PAYLOAD_TYPE_REPRESENTATION)
+    {
+        throw "Incoming payload is NULL or not a representation";
+    }
+
+    OCRepPayload *input = reinterpret_cast<OCRepPayload *>(ehRequest->payload);
+    if (!input)
+    {
+        throw "PUT payload is null";
+    }
+
+    if (UPNP_OIC_TYPE_POWER_SWITCH == resourceType)
+    {
+        if (!OCRepPayloadGetPropBool(input, powerSwitchStateName, &powerSwitchStateValue))
+        {
+            throw "No power switch value in request payload";
+        }
+        DEBUG_PRINT("New " << powerSwitchStateName << ": " << (powerSwitchStateValue ? "true" : "false"));
+
+        //TODO set actual value in UPnP light
+
+        if (!OCRepPayloadSetPropBool(payload, powerSwitchStateName, powerSwitchStateValue))
+        {
+            throw "Failed to set power switch value in payload";
+        }
+        DEBUG_PRINT(powerSwitchStateName << ": " << (powerSwitchStateValue ? "true" : "false"));
+    }
+    else
+    {
+        throw "Failed due to unknown resource type" ;
+    }
+
+    return OC_EH_OK;
+}
