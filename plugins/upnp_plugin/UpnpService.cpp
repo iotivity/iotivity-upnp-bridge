b2d5a3fe9455c3aa846f47b1f654fdcdfd5ae5e2
Larry Sachs <larry.j.sachs@intel.com>
2017-07-13 14:59:11 -0700

    Ensure cached service proxy is still valid
    Change-Id: I7c07bfc62e220cf67c395f954b3f438130aa5daf
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

--- plugins/upnp_plugin/UpnpService.cpp
+++ plugins/upnp_plugin/UpnpService.cpp
@@ -171,51 +171,58 @@ OCEntityHandlerResult UpnpService::processGetRequest(string uri, OCRepPayload *p
     if (UPNP_SERVICE_RESOURCE == resourceType)
     {
         GUPnPServiceInfo *serviceInfo = GUPNP_SERVICE_INFO(m_proxy);
-
-        const char *serviceType = gupnp_service_info_get_service_type(serviceInfo);
-        if (OCRepPayloadSetPropString(payload, SERVICE_TYPE.c_str(), serviceType))
+        if (GUPNP_IS_SERVICE_INFO(serviceInfo))
         {
-            DEBUG_PRINT(SERVICE_TYPE << ": " << serviceType);
+            const char *serviceType = gupnp_service_info_get_service_type(serviceInfo);
+            if (OCRepPayloadSetPropString(payload, SERVICE_TYPE.c_str(), serviceType))
+            {
+                DEBUG_PRINT(SERVICE_TYPE << ": " << serviceType);
+            }
+            else
+            {
+                ERROR_PRINT("Failed to set " + SERVICE_TYPE << " value in payload");
+            }
+
+            for (auto const &kv : s_serviceInfo2AttributesMap)
+            {
+                char *c_field = kv.second(serviceInfo);
+                if (c_field != NULL)
+                {
+                    if (OCRepPayloadSetPropString(payload, kv.first.c_str(), c_field))
+                    {
+                        DEBUG_PRINT(kv.first << ": " << c_field);
+                        g_free(c_field);
+                    }
+                    else
+                    {
+                        ERROR_PRINT("Failed to set " << kv.first << " value in payload");
+                    }
+                }
+            }
+
+            if (!m_links.empty())
+            {
+                DEBUG_PRINT("Setting links for " << uri);
+                const OCRepPayload *links[m_links.size()];
+                size_t dimensions[MAX_REP_ARRAY_DEPTH] = {m_links.size(), 0, 0};
+                for (unsigned int i = 0; i < m_links.size(); ++i) {
+                    DEBUG_PRINT(OC_RSRVD_LINKS << "[" << i << "]");
+                    DEBUG_PRINT("\t" << OC_RSRVD_HREF << "=" << m_links[i].href);
+                    DEBUG_PRINT("\t" << OC_RSRVD_REL << "=" << m_links[i].rel);
+                    DEBUG_PRINT("\t" << OC_RSRVD_RESOURCE_TYPE << "=" << m_links[i].rt);
+                    OCRepPayload *linkPayload = OCRepPayloadCreate();
+                    OCRepPayloadSetPropString(linkPayload, OC_RSRVD_HREF, m_links[i].href.c_str());
+                    OCRepPayloadSetPropString(linkPayload, OC_RSRVD_REL, m_links[i].rel.c_str());
+                    OCRepPayloadSetPropString(linkPayload, OC_RSRVD_RESOURCE_TYPE, m_links[i].rt.c_str());
+                    links[i] = linkPayload;
+                }
+                OCRepPayloadSetPropObjectArray(payload, OC_RSRVD_LINKS, links, dimensions);
+            }
         }
         else
         {
-            ERROR_PRINT("Failed to set " + SERVICE_TYPE << " value in payload");
-        }
-
-        for (auto const &kv : s_serviceInfo2AttributesMap)
-        {
-            char *c_field = kv.second(serviceInfo);
-            if (c_field != NULL)
-            {
-                if (OCRepPayloadSetPropString(payload, kv.first.c_str(), c_field))
-                {
-                    DEBUG_PRINT(kv.first << ": " << c_field);
-                    g_free(c_field);
-                }
-                else
-                {
-                    ERROR_PRINT("Failed to set " << kv.first << " value in payload");
-                }
-            }
-        }
-
-        if (!m_links.empty())
-        {
-            DEBUG_PRINT("Setting links for " << uri);
-            const OCRepPayload *links[m_links.size()];
-            size_t dimensions[MAX_REP_ARRAY_DEPTH] = {m_links.size(), 0, 0};
-            for (unsigned int i = 0; i < m_links.size(); ++i) {
-                DEBUG_PRINT(OC_RSRVD_LINKS << "[" << i << "]");
-                DEBUG_PRINT("\t" << OC_RSRVD_HREF << "=" << m_links[i].href);
-                DEBUG_PRINT("\t" << OC_RSRVD_REL << "=" << m_links[i].rel);
-                DEBUG_PRINT("\t" << OC_RSRVD_RESOURCE_TYPE << "=" << m_links[i].rt);
-                OCRepPayload *linkPayload = OCRepPayloadCreate();
-                OCRepPayloadSetPropString(linkPayload, OC_RSRVD_HREF, m_links[i].href.c_str());
-                OCRepPayloadSetPropString(linkPayload, OC_RSRVD_REL, m_links[i].rel.c_str());
-                OCRepPayloadSetPropString(linkPayload, OC_RSRVD_RESOURCE_TYPE, m_links[i].rt.c_str());
-                links[i] = linkPayload;
-            }
-            OCRepPayloadSetPropObjectArray(payload, OC_RSRVD_LINKS, links, dimensions);
+            ERROR_PRINT("GUPNP_SERVICE_INFO failed for proxy for " << m_uri);
+            return OC_EH_ERROR;
         }
     }
 
