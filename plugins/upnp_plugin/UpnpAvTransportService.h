93fa1a6b2c29fd474b3be9451af33cf677dcaacb
Larry Sachs <larry.j.sachs@intel.com>
2018-02-14 13:48:04 -0800

    Beginnings of OCF AV Data Models
    Change-Id: I6ffbf3c9fd7cc6f8fe09e0621a1b02e8a6986bdd
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

---
+++ plugins/upnp_plugin/UpnpAvTransportService.h
@@ -0,0 +1,54 @@
+//******************************************************************
+//
+// Copyright 2018 Intel Corporation All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef UPNP_AV_TRANSPORT_SERVICE_H_
+#define UPNP_AV_TRANSPORT_SERVICE_H_
+
+#include <string>
+#include <map>
+
+#include <gupnp.h>
+
+#include "UpnpResource.h"
+#include "UpnpInternal.h"
+#include "UpnpService.h"
+
+using namespace std;
+
+class UpnpAvTransport: public UpnpService
+{
+        friend class UpnpService;
+
+    public:
+        UpnpAvTransport(GUPnPServiceInfo *serviceInfo,
+                        UpnpRequestState *requestState):
+            UpnpService(serviceInfo, UPNP_OIC_TYPE_AV_TRANSPORT, requestState, &Attributes)
+        {
+        }
+
+        OCEntityHandlerResult processGetRequest(string uri, OCRepPayload *payload, string resourceType);
+        OCEntityHandlerResult processPutRequest(OCEntityHandlerRequest *ehRequest,
+                    string uri, string resourceType, OCRepPayload *payload);
+
+    private:
+        static vector <UpnpAttributeInfo> Attributes;
+};
+
+#endif
